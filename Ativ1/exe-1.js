4aa9bdccc76b25cbde195aeef0311df9dfa9195c
RangerTl <110108397+RangerTl@users.noreply.github.com>
2022-11-28 18:44:17 -0300

    Add files via upload

---
+++ Ativ1/exe-1.js
@@ -0,0 +1,18 @@
+//Calcule a média de diversas notas digitadas pelo usuário.
+
+
+// média = aritmética (ex : 5 10 15 20 25) / contagem dos numeros 
+// ex : (5+10+15+20+25) / 5
+//  75 / 5 
+//media = (n1 + n2 + n3)/3 ; 
+var  n1 = Number(window.prompt("digite o primeiro "))//,n2,n3,n4,n5  ,vez
+var  n2 = Number(window.prompt("digite o segundo valor "))//,n2,n3,n4,n5  ,vez
+var  n3 = Number(window.prompt("digite o terceiro valor "))//,n2,n3,n4,n5  ,vez
+var  n4 = Number(window.prompt("digite o quarto valor "))//,n2,n3,n4,n5  ,vez
+var  n5 = Number(window.prompt("digite o quinto valor "))//,n2,n3,n4,n5  ,vez
+    
+var total = [n1 + n2 + n3 + n4 + n5] / 5;  
+//let = i = window.prompt("digite as vezes a se reptir :"); 
+
+
+alert(total);
---
+++ Ativ2/exe-2.js
@@ -0,0 +1,5 @@
+var val = window.prompt("digite o primeiro valor :");
+val = (Math.sqrt(val));
+
+alert("sua raiz é :"+val);
+
